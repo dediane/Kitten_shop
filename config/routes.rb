9030a81a444abf574f6098653d4d15b059eea328
Rémi BARNAT <rbarnat@gmail.com>
2020-11-24 11:17:46 +0400

    Add User controller + routes users#show + view users/show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
-  resources :line_items
-  resources :carts
-  resources :orders
-  devise_for :users
+  # HOME PAGE
   root to: "items#index"
+
+  # USERS AND PROFILE PAGES
+  resources :users, only: [:show]
+  devise_for :users
+
+  # ITEMS PAGES
   resources :items
+
+  # LINE ITEMS PAGES
+  resources :line_items
+
+  # CART PAGES
+  resources :carts
+
+  #ORDER PAGES
+  resources :orders
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
