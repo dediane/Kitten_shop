eff9858aa3bf3eddab170d432a25eacf39a66798
Mehdi-Lucas <71726210+Mehdi-Lucas@users.noreply.github.com>
2020-11-24 14:03:43 +0100

    Revert "Added stripe with default behavior"

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,5 @@ Rails.application.routes.draw do
   #ORDER PAGES
   resources :orders
   
-  # STRIPE CHARGES PAGES
-  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
